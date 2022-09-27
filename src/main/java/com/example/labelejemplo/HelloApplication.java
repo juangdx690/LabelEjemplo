c5be9a481f3530bbb3dbfdf9921ad83c350a15cc
juan <juanguadix69@gmail.com>
2022-09-27 02:16:37 +0200

    segunda subida

--- src/main/java/com/example/labelejemplo/HelloApplication.java
+++ src/main/java/com/example/labelejemplo/HelloApplication.java
@@ -1,19 +1,31 @@
 package com.example.labelejemplo;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    EventHandler<MouseEvent> cierra = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            System.exit(0);
+        }
+    };
+
     @Override
     public void start(Stage stage) throws IOException {
         //así se crea un label
         Label lbl = new Label("Esto es un label");
+        lbl.addEventHandler(MouseEvent.MOUSE_CLICKED, cierra);
         StackPane root = new StackPane();
         Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Ejemplo Label!");
